971c1605e3c6ebba6588d619ac6ed5a8f5218812
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-03-16 13:46:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+ <div id="app">
+     <Navbar />
+        <div class="container">
+<router-view/>
+        </div>
+        </div>
+  <Footer/>
+</template>
+
+
+<script>
+import Footer from './components/Footer.vue'
+import Navbar from './components/Navbar.vue'
+export default {
+  components: { Navbar, Footer },
+ 
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #eee;
+}
+.error-feedback{
+  color: rgb(161, 38, 38);
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: home
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: home
+  }
+  ,
+  {
+    path: '/about',
+    name: 'about',
+    component: about
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: login
+  },
+  {
+    path: '/register',
+    name: 'about',
+    component: about
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
