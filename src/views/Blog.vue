8dc9526fa3e3bd1dc4e8fd7f23c6a7e5a32f8686
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-03-23 14:45:34 +0200

    loader and b log

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,68 +1,40 @@
 <template>
+
 <Search/>
 
-<div class="container-fluid">
-  <div class="col-lg-6 col-md-6 hidden-sm hidden-xs">
-    <figure>
-      <div class="media" style="background-image:url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/584938/bg_5.png)"></div>
-      <figcaption>
-        <svg viewBox="0 0 200 200" version="1.1" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <mask id="mask" x="0" y="0" width="100%" height="100%">
-              <rect id="alpha" x="0" y="0" width="100%" height="100%"></rect>
-              <text class="title" dx="50%" dy="2.5em">ENJOY</text>
-              <text class="title" dx="50%" dy="3.5em">EVERY</text>
-              <text class="title" dx="50%" dy="4.5em">MOMENT</text>
-            </mask>
-          </defs>
-          <rect id="base" x="0" y="0" width="100%" height="100%"></rect>
-        </svg>
-        <div class="body">
-          <p>Enamel pin selvage health goth edison bulb, venmo glossier tattooed hella butcher cred iPhone. Plaid skateboard man braid wayfarers.</p>
-        </div>
-      </figcaption><a href="#"></a>
-    </figure>
-  </div>
-</div>
 
-
-
-
-   <div v-if="posts">
-    <h2>Blogs</h2>
+ <div v-if="posts"> 
+  <h2>Blogs</h2>
     <div class="posts-container" v-if="posts">
-      <div 
-        v-for="post of posts"
-        :key="post._id"
-        >
+      <div v-for="post of posts" :key="post._id" >
 
+<div class="container">
+  <div class="row">
+    <div class="col-md-6 item">
+      <div class="item-in">
+        <h4>{{post.title}}</h4>
+        <div class="seperator"></div>
+        <p>{{post.description}}</p>
+         <router-link class="read-more" :to="{ name: 'BlogDetails', params: { id: post._id } }" >Read more<i class="fa fa-long-arrow-right"></i></router-link>
 
-        <div>{{post.description}}</div>
-        <div>{{post.category}}</div>
-        <div>{{post.date}}</div>
-        <img :src="post.img" :alt="post.title" />
-        {{ post.author}}
-        <router-link :to="{ name: 'BlogDetails', params: { id: post._id } }" >Read more</router-link>
+          
+      </div>
       </div>
     </div>
-
-
-
- <button>
-   <router-link :to="{name:'createblog'}" class="nav-link"> Create blog</router-link>
+   
+    </div>
+    </div>
+    </div>
+    <button>
+   <router-link  :to="{name:'createblog'}" class="nav-link"> Create blog</router-link>
  </button>
+    </div>
+    
 
-  </div>
-
-
-  <div v-else>Loading blogs... 
-    <div class="flower-spinner">
-      <div class="dots-container">
-        <div class="bigger-dot">
-          <div class="smaller-dot"></div>
-        </div>
-      </div>
-    </div>  
+  
+  <div v-else>
+    Loading blogs... 
+    <Loader/>
  </div>
 
 
@@ -70,8 +42,10 @@
 
 <script>
 import Search from '../components/Search.vue'
+import Loader from '../components/Loader.vue'
+
 export default {
-  components: { Search },
+  components: { Search, Loader },
  data() {
     return {
       posts: null,
@@ -88,193 +62,152 @@ export default {
 </script>
 
 <style scoped>
-.flower-spinner,  .flower-spinner * {
-      box-sizing: border-box;
-    }
-
-    .flower-spinner {
-      height: 70px;
-      width: 70px;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
-    }
-
-    .flower-spinner .dots-container {
-      height: calc(70px / 7);
-      width: calc(70px / 7);
-    }
-
-    .dots-container {
-      margin-left: 50%;
-    }
-
-    .flower-spinner .smaller-dot {
-      background: #56ab2f;
-      height: 100%;
-      width: 100%;
-      border-radius: 50%;
-      animation: flower-spinner-smaller-dot-animation 2.5s 0s infinite both;
-
-    }
-
-    .flower-spinner .bigger-dot {
-      background: #56ab2f;
-      height: 100%;
-      width: 100%;
-      padding: 10%;
-      border-radius: 50%;
-      animation: flower-spinner-bigger-dot-animation 2.5s 0s infinite both;
-    }
-
-    @keyframes flower-spinner-bigger-dot-animation {
-      0%, 100% {
-        box-shadow: #56ab2f,
-       #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px;
-      }
-
-      50% {
-        transform: rotate(180deg);
-      }
-
-      25%, 75% {
-        box-shadow: #56ab2f 26px 0px 0px,
-        #56ab2f -26px 0px 0px,
-        #56ab2f 0px 26px 0px,
-        #56ab2f 0px -26px 0px,
-        #56ab2f 19px -19px 0px,
-        #56ab2f 19px 19px 0px,
-        #56ab2f -19px -19px 0px,
-        #56ab2f -19px 19px 0px;
-      }
-
-      100% {
-        transform: rotate(360deg);
-        box-shadow: #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px;
-      }
-    }
-
-    @keyframes flower-spinner-smaller-dot-animation {
-      0%, 100% {
-        box-shadow: #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px;
-      }
-
-      25%, 75% {
-        box-shadow: #56ab2f 14px 0px 0px,
-        #56ab2f -14px 0px 0px,
-        #56ab2f 0px 14px 0px,
-        #56ab2f 0px -14px 0px,
-        #56ab2f 10px -10px 0px,
-        #56ab2f 10px 10px 0px,
-        #56ab2f -10px -10px 0px,
-        #56ab2f -10px 10px 0px;
-      }
-
-      100% {
-        box-shadow: #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px,
-        #56ab2f 0px 0px 0px;
-      }
-    }
-
-
-* {
-  box-sizing: border-box;
-}
-
 
 /* card */
-.container-fluid {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translateY(-50%) translateX(-50%);
-  width: 100%;
+@import url(https://fonts.googleapis.com/css?family=Merriweather:400,300,700);
+@import url(https://fonts.googleapis.com/css?family=Montserrat:400,700);
+body {
+  background: #fbfbfb;
+  font-family: "Merriweather", serif;
+  font-size: 16px;
+  color: #777;
 }
-.media {
-  background-position: center;
-  background-size: cover;
-  height: 100%;
-  position: absolute;
-  transition: all 0.3s ease;
-  width: 100%;
+
+h1, h4 {
+  font-family: "Montserrat", sans-serif;
 }
-figure {
-  height: 370px;
-  overflow: hidden;
+
+.row {
+  padding: 50px 0;
+}
+
+.seperator {
+  margin-bottom: 30px;
+  width: 35px;
+  height: 3px;
+  background: #777;
+  border: none;
+}
+
+.title {
+  text-align: center;
+}
+.title .row {
+  padding: 50px 0 0;
+}
+.title h1 {
+  text-transform: uppercase;
+}
+.title .seperator {
+  margin-top: 0;
+  margin-left:50%;
+}
+
+.item {
+  position: relative;
+  margin-bottom: 30px;
+  min-height: 1px;
+  float: left;
+  /* -webkit-backface-visibility: hidden; */
+  -webkit-tap-highlight-color: transparent;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.item .item-in {
+  background: #fff;
+  padding: 40px;
   position: relative;
 }
-figure a {
-  height: 100%;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 100%;
-  z-index: 3;
-}
-figure:hover .media {
-  transform: scale(1.25);
-}
-figcaption {
-  color: #252830;
-  height: calc(100% - 30px);
-  margin: 15px;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 250px;
-}
-.body {
-  background-color: white;
-  bottom: 0;
-  padding: 15px;
-  position: absolute;
+.item .item-in:hover:before {
   width: 100%;
 }
-svg {
-  height: inherit;
-  width: 100%;
+.item .item-in::before {
+  content: "";
+  position: absolute;
+  bottom: 0px;
+  height: 2px;
+  width: 0%;
+  background: #333333;
+  right: 0px;
+  transition: width 0.4s;
 }
-svg text {
-  text-anchor: middle;
+
+.item h4 {
+  font-size: 18px;
+  margin-top: 25px;
+  letter-spacing: 2px;
+  text-transform: uppercase;
 }
-svg #alpha {
-  fill: white;
+.item p {
+  font-size: 12px;
 }
-svg .title {
-  font-size: 28px;
-  font-family: "Montserrat";
-  letter-spacing: 5px;
+.item a {
+  font-family: "Montserrat", sans-serif;
+  font-size: 12px;
+  text-transform: uppercase;
+  color: #666666;
+  margin-top: 10px;
 }
-svg #base {
-  fill: white;
-  -webkit-mask: url(#mask);
-  mask: url(#mask);
+.item a i {
+  opacity: 0;
+  padding-left: 0px;
+  transition: 0.4s;
+  font-size: 24px;
+  display: inline-block;
+  top: 5px;
+  position: relative;
 }
+.item a:hover {
+  text-decoration: none;
+}
+.item a:hover i {
+  padding-left: 10px;
+  opacity: 1;
+  font-weight: 300;
+}
+
+.item .icon {
+  position: absolute;
+  top: 27px;
+  left: -16px;
+  cursor: pointer;
+}
+.item .icon a {
+  font-family: "Merriweather", serif;
+  font-size: 14px;
+  font-weight: 400;
+  color: #999;
+  text-transform: none;
+}
+.item .icon svg {
+  width: 32px;
+  height: 32px;
+  float: left;
+}
+.item .icon .icon-topic {
+  opacity: 0;
+  padding-left: 0px;
+  transition: 0.4s;
+  display: inline-block;
+  top: 0px;
+  position: relative;
+}
+.item .icon:hover .icon-topic {
+  opacity: 1;
+  padding-left: 10px;
+}
+
+@media only screen and (max-width: 768px) {
+  .item .icon {
+    position: relative;
+    top: 0;
+    left: 0;
+  }
+}
+
+
+
 </style>
