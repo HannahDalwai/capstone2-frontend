fc0f27c6ea31739cfad453c2868c9c6ee0986927
HannahDalwai <hannahdalwai4@gmail.com>
2022-03-26 13:32:41 -0700

    ahhhh

--- src/views/AdminTester.vue
+++ src/views/AdminTester.vue
@@ -53,9 +53,96 @@
 <!-- sort and search -->
  
 
- 
+
+
+ <h1>users</h1>
 <!-- users table -->
-      
+<table >
+  <tr>
+    <th>user</th>
+    <th>email</th>
+    <th>contact</th>
+    <th>member since</th>
+    <th>actions</th>
+    
+
+  </tr>
+    <tr v-for="user of filteredUsers" :key="user._id">
+      <td >
+         {{user.fullname}}
+        </td>
+      <td >
+        {{user.phone_number}}
+      </td>
+      <td >
+        {{user.email}}
+      </td>
+      <td >
+        {{user.join_date}}
+      </td>
+      <td >
+        <button>edit</button>
+      </td>
+       <td >
+        <button>delete</button>
+      </td>
+     
+      <!-- <td >
+        {{user.password}}
+      </td> --> 
+       <!-- <td >
+        role
+      </td> -->
+
+    </tr>
+  </table>
+
+
+
+
+<h1>blogs</h1>
+<!-- blogs table -->
+
+<table >
+  <tr>
+    <th>title</th>
+    <th>category</th>
+    <th>description</th>
+    <th>img url</th>
+    <th>actions</th>
+    
+
+  </tr>
+    <tr -for="post of filteredPosts" :key="post._id" >
+      <td >
+         {{user.fullname}}
+        </td>
+      <td >
+        {{user.phone_number}}
+      </td>
+      <td >
+        {{user.email}}
+      </td>
+      <td >
+        {{user.join_date}}
+      </td>
+      <td >
+        <button>edit</button>
+      </td>
+       <td >
+        <button>delete</button>
+      </td>
+     
+      <!-- <td >
+        {{user.password}}
+      </td> --> 
+       <!-- <td >
+        role
+      </td> -->
+
+    </tr>
+  </table>
+
 
 
 
@@ -84,6 +171,7 @@ export default {
       posts: null,
       users: null,
       filteredBlogs: null,
+      filteredUsers: null,
       title: "",
       search: "",
       fullname: "",
@@ -176,5 +264,194 @@ computed: {
 };
 </script>
 <style scoped>
+.container {
+   width: 950px;
+   margin: auto;
+}
+
+
+
+table {
+   width: 100%;
+}
+
+th, td {
+   padding-top: 15px;
+   padding-bottom: 15px;
+   padding-left: 15px;
+}
+
+th {
+   text-align: left;
+   background-color: #F2F2F2;
+   color: #65696B;
+}
+
+tr:nth-child(odd) {
+   background: #F9F9F9;
+   border-top: 1px solid transparent;
+   border-left: 1px solid transparent;
+}
+
+tr:nth-child(even) {
+   background: #FFF;
+   border-top: 1px solid transparent;
+   border-left: 1px solid transparent;
+}
+
+tr:hover {
+   background-color: #EBF5FB;
+   cursor: default;
+   border: 1px solid #67B2E4;
+}
+
+.username {
+   color: #3498DB;
+}
+
+.username:hover, a:hover {
+   color: #3498DB;
+   cursor: pointer;
+   text-decoration: underline;
+}
+
+/* .activeUser {
+   color: #03AB61;
+} */
+
+/* .bannedUser, .suspendedUser {
+   font-weight: 600;
+} */
+.bannedUser {
+   color: red;
+}
+
+.suspendedUser {
+   color: #FF8C00;
+}
+
+.commenter {
+   color: #9A9DA0;
+}
+
+.admin, .staff, .moderator {
+   font-weight: 600;
+}
+
+a {
+   color: #3498DB;
+   text-decoration: none;
+}
+
+nav {
+   padding-left: 20px;
+   padding-right: 20px;
+   border-bottom: 1px solid #E9E9E9;
+}
+nav ul li {
+   display: inline-block;
+   font-weight: 600;
+   font-size: 16px;
+   text-transform: uppercase;
+}
+
+nav ul li {
+  text-align: center;
+  padding: 20px 16px;
+  text-decoration: none;
+}
+
+.talk {
+   text-transfor: uppercase;
+   font-size: 24px;
+}
+
+.active {
+   background-color: #F77160;
+   color: white;
+}
+
+.roleHead {
+   width: 115px;
+}
+
+.statusHead {
+   width: 115px;
+}
+
+i {
+   float: right;
+   padding: 0px 15px;
+   color: #787D80;
+   font-size: 12px;
+}
+
+td.activeUser:hover, td.suspendedUser:hover, td.bannedUser:hover, td.commenter, td.staff, td.admin, td.moderator {
+   cursor: pointer;
+}
+
+tr.table-header {
+  border: 1px solid #BBBEBF;
+}
+
+.styled-select select {
+   background: transparent;
+   border: none;
+   font-size: 14px;
+   font-family: 'Source Sans Pro', sans-serif;
+   height: 29px;
+   padding: 5px; /* If you add too much padding here, the options won't show in IE */
+   width: 100%;
+}
+
+.styled-select {
+   margin-top: 5px;
+   width:200px;
+   border-radius: 2px;
+   border: 1px solid #9A9DA0;
+   color: #3B4A53;
+}
+
+.filter-ui, .search-ui {
+   display: inline-block;
+}
+
+input[type=text] {
+  padding: 6px;
+  margin-top: 5px;
+  font-size: 14px;
+  width: 300px;
+  border: 1px solid #9A9DA0;
+  border-radius: 2px;
+}
+
+.search-container button {
+  padding: 10px 5px;
+  margin-right: 16px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  border-radius: 2px;
+  cursor: pointer;
+}
+
+
+::-webkit-input-placeholder {
+  color: #BBBEBF;
+  font-size: 13px;
+}
+::-moz-placeholder {
+  color: #BBBEBF;
+  font-size: 13px;
+}
+:-ms-input-placeholder {
+  color: #BBBEBF;
+  font-size: 13px;
+}
+::placeholder {
+  color: #BBBEBF;
+  font-size: 13px;
+}
+
 
 </style>
